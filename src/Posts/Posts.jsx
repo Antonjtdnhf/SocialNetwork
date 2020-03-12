95ed2dbef83042ba78baad8e57a5a1db6ba3da0c
Antonjtdnhf <antondelaushiy@gmail.com>
2020-03-12 22:07:58 +0300

    added map method into the posts

--- src/Posts/Posts.jsx
+++ src/Posts/Posts.jsx
@@ -3,14 +3,26 @@ import s from './Posts.module.css';
 import Add_new_post from "./Add_new_post/Add_new_post";
 import Post from './Post/Post.jsx';
 
+
+let postsData = [
+   { message : "You didn't see anything", likesCount : "15"},
+   { message : "react top", likesCount : "13"},
+   { message : "i'm impressive", likesCount : "8"},
+   { message : "go next", likesCount : "80"},
+]
+
+let postsElements = postsData.map( (p) => {
+    return (
+        <Post message={p.message} likesCount={p.likesCount}/>
+    )
+})
+
+
 const Posts = () => {
     return (
         <div className={s.posts}>
             <Add_new_post/>
-            <Post message="You didn't see anything" likesCount="15"/>
-            <Post message="react top" likesCount="13"/>
-            <Post message="i'm impressive" likesCount="8"/>
-            <Post message="go next" likesCount="80"/>
+            {postsElements}
         </div>
     );
 }
