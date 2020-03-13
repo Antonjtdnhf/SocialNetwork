a97ae6f42f8166d4644eb2a3fc7fdc5ad90a86f3
Antonjtdnhf <antondelaushiy@gmail.com>
2020-03-13 09:12:06 +0300

    moved all components in components drectory, create state.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import './App.css';
-import Header from './Header/Header';
-import './Header/Header.module.css';
-import About_me from './About_me/About_me';
-import './About_me/About_me.module.css';
-import Posts from "./Posts/Posts";
-import './Posts/Posts.module.css';
-import Last_photos from './Last_photos/Last_photos';
-import './Last_photos/Last_photos.module.css';
-import Footer from './Footer/Footer';
-import './Footer/Footer.module.css';
-import Messages from "./Messages/Messages";
+import Header from './Components/Header/Header';
+import './Components/Header/Header.module.css';
+import About_me from './Components/About_me/About_me';
+import './Components/About_me/About_me.module.css';
+import Posts from "./Components/Posts/Posts";
+import './Components/Posts/Posts.module.css';
+import Last_photos from './Components/Last_photos/Last_photos';
+import './Components/Last_photos/Last_photos.module.css';
+import Footer from './Components/Footer/Footer';
+import './Components/Footer/Footer.module.css';
+import Messages from "./Components/Messages/Messages";
 import {Route , BrowserRouter} from "react-router-dom";
-import Videos from "./Videos/Videos";
-import Photos from "./Photos/Photos";
-import Profile from "./Profile/Profile";
+import Videos from "./Components/Videos/Videos";
+import Photos from "./Components/Photos/Photos";
+import Profile from "./Components/Profile/Profile";
 
 
 const App = (props) => {
